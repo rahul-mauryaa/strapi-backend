1ff0650942f70b44b43021f22af59ba54aec9325
Rahul Maurya <118436435+rahul-mauryaa@users.noreply.github.com>
2024-02-27 09:16:37 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,16 @@
-// ./config/env/production/plugins.js
 
-module.exports = {
-    graphql: {
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: true,
-        depthLimit: 10,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-          introspection: true,
-        },
+export default {
+  //
+  graphql: {
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      // depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
-  };
+  },
+};
