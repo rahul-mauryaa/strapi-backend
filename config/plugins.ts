04460034974e0a3e18a80e42c5b7ce4820f9b2b5
Rahul Maurya <118436435+rahul-mauryaa@users.noreply.github.com>
2024-02-28 17:43:18 +0530

    new plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,7 +4,7 @@ module.exports = {
       endpoint: '/graphql',
       shadowCRUD: true,
       playgroundAlways: true,
-      depthLimit: 10,
+      // depthLimit: 10,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
